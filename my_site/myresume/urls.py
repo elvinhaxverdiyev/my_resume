ee0e211ea04f540dc43e28faef54fee5516423b6
Elvin Haxverdiyev <elvinhaxverdiyev777@github.com>
2025-12-17 17:22:17 +0400

    created image model and created home and projects view

---
+++ my_site/myresume/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('project/<int:project_id>/', ProjectsDetailView.as_view(), name='project_detail'),
+]
