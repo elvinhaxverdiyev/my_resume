3bba9ed261fce3f131aa6e126943080aa0683161
Elvin Haxverdiyev <elvinhaxverdiyev777@github.com>
2025-12-18 12:00:53 +0400

    added some updates

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -5,12 +5,16 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myresume.urls'))
+    path(
+        'admin/',
+        admin.site.urls
+    ),
+    path(
+        '',
+        include('myresume.urls')
+    )
 ]
 
-# Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    # Serve static files in development (Django handles this automatically, but this makes it explicit)
     urlpatterns += staticfiles_urlpatterns()
